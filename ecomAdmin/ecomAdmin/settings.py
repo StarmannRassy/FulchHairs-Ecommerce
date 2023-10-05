374bf1322a3320ed35c688bf57955b80e25a3392
StarmannRassy <boronicle@gmail.com>
2023-10-05 02:12:29 +0000

    allowed hosts

--- ecomAdmin/ecomAdmin/settings.py
+++ ecomAdmin/ecomAdmin/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".versel.app", ".now,sk", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [".versel.app", ".now,sk", "127.0.0.1", "localhost", "https://fulch-webappv2.onrender.com"]
 
 
 
